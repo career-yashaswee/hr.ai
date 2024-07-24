4e9d85ea80d8015bb8c11972268b8e20d8f4da62
Nainshi Verma <122629057+nainshiverma@users.noreply.github.com>
2024-07-24 22:27:57 +0530

    Update routes of interviewer.js

--- server/routes/interviewer.js
+++ server/routes/interviewer.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getInterviewer } = require('../controllers/interviewer');
+const { getInterviewer, addInterviewer, deleteInterviewer } = require('../controllers/interviewer');
 
 router.get('/interviewerbyID/', getInterviewer);
+router.post('/:addinterviewerbyID', addInterviewer);
+router.delete('/:deleteinterviewerbyID', deleteInterviewer);
 module.exports = router;
